249658741b1d82275d606dc864ddbaaa06a3f959
why-iskra <75670546+why-iskra@users.noreply.github.com>
2022-01-12 02:24:03 +0700

    increase version

--- buildSrc/src/main/kotlin/ApplicationInfo.kt
+++ buildSrc/src/main/kotlin/ApplicationInfo.kt
@@ -3,7 +3,7 @@ object ApplicationInfo {
     object Version {
         const val MAJOR = 2
         const val MINOR = 9
-        const val PATCH = 0
+        const val PATCH = 1
         const val CODE = 11
     }
 
