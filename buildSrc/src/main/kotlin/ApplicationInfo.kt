ba34c928858047d620682303eb9aacf98f154035
why-iskra <v_iskortsev@mail.ru>
2022-01-17 06:28:39 +0700

    increase version

--- buildSrc/src/main/kotlin/ApplicationInfo.kt
+++ buildSrc/src/main/kotlin/ApplicationInfo.kt
@@ -3,8 +3,8 @@ object ApplicationInfo {
     object Version {
         const val MAJOR = 2
         const val MINOR = 9
-        const val PATCH = 3
-        const val CODE = 12
+        const val PATCH = 4
+        const val CODE = 13
     }
 
 }
